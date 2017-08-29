c0726aad6319f622db65d4e0eb13731d1b50d6d8
simon <SimonSayID@outlook.com>
2017-08-29 10:48:30 +0800

    add avl_tree_test and fix stack memory leak

--- avl_tree.c
+++ avl_tree.c
@@ -58,7 +58,7 @@ avl_node_t* avl_insert(avl_node_t *node, int data){
         new->data = data;
         new->left = new->right = NULL;
         new->height = 0;
-        stacks_t *stacks = stacks_avl_init();
+        stack_avl_t *stacks = stacks_avl_init();
         int flag = 0;
         do {
             if (node->data > data) {
@@ -132,7 +132,7 @@ avl_node_t* avl_insert(avl_node_t *node, int data){
 avl_node_t* avl_delete(avl_node_t *node, int data){
     if (node != NULL) {
         int rotation_flag = 0;
-        stacks_t *stacks = stacks_avl_init();
+        stack_avl_t *stacks = stacks_avl_init();
         while (node != NULL && node->data != data){
             if (node->data > data) {
                 stacks_avl_push(stacks, node);
@@ -243,7 +243,9 @@ avl_node_t* avl_delete(avl_node_t *node, int data){
             node = check;
         }
         end:
+        stacks_avl_clean(stacks);
         free(stacks);
+        stacks = NULL;
         return node;
     }
 }
@@ -263,4 +265,39 @@ avl_node_t* avl_search(avl_node_t *node, int data){
         return node;
     }
     return NULL;
+}
+
+void avl_tree_test(){
+    avl_node_t *root = avl_tree_init(1);
+    root = avl_insert(root, 2);
+    root = avl_insert(root, 3);
+    root = avl_insert(root, 4);
+    root = avl_insert(root, 5);
+    root = avl_insert(root, 6);
+    root = avl_insert(root, 7);
+    root = avl_insert(root, 8);
+    root = avl_insert(root, 9);
+    root = avl_insert(root, 10);
+    root = avl_insert(root, 11);
+    root = avl_insert(root, 12);
+    root = avl_insert(root, 13);
+    root = avl_insert(root, 14);
+    root = avl_insert(root, 15);
+    root = avl_insert(root, 16);
+    root = avl_insert(root, 17);
+    root = avl_insert(root, 18);
+    root = avl_insert(root, 19);
+    root = avl_insert(root, 20);
+    root = avl_insert(root, 21);
+    root = avl_insert(root, 22);
+    root = avl_delete(root, 17);
+    root = avl_delete(root, 15);
+    root = avl_delete(root, 10);
+    root = avl_delete(root, 11);
+    root = avl_delete(root, 12);
+    avl_node_t *node = avl_search(root, 22);
+    node = avl_search(root, 20);
+    node = avl_search(root, 17);
+    node = avl_search(root, 12);
+    node = avl_search(root, 3);
 }
