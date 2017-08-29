d3f473d41eaefd153a5b74b2bb485ed898e11334
simon <SimonSayID@outlook.com>
2017-08-29 10:56:12 +0800

    add general stack and stack clean function (fix memory leak)

--- stacks.c
+++ stacks.c
@@ -2,17 +2,91 @@
 // Created by simon on 8/27/17.
 //
 
+#include <assert.h>
 #include "main.h"
 
-stacks_t* stacks_avl_init(){
-    stacks_t *stacks = (stacks_t *)malloc(sizeof(stacks_t));
+stacks_t* stack_init(){
+    stacks_t *stack = (stacks_t *) malloc(sizeof(stack));
+    stack->data = NULL;
+    stack->next = NULL;
+    return stack;
+}
+
+void stack_push(stacks_t* stack, void *data){
+    if (stack != NULL) {
+        stacks_t *new = (stacks_t *) malloc(sizeof(stacks_t));
+        new->data = data;
+        new->next = NULL;
+        stack->next = new;
+    } else {
+        return;
+    }
+}
+
+stacks_t* stack_top(stacks_t* stack){
+    if (stack != NULL) {
+        stacks_t *res = stack->next;
+        return res;
+    }
+    return NULL;
+}
+
+void stack_pop(stacks_t *stack){
+    if (stack != NULL) {
+        stacks_t *del = stack->next;
+        stack->next = stack->next->next;
+        free(del);
+    }
+}
+
+void stack_clean(stacks_t *stack){
+    if (stack != NULL) {
+        while (stack->next != NULL) {
+            stack_pop(stack);
+        }
+    } else {
+        return;
+    }
+}
+
+void stack_test(){
+    /* data */
+    int *a = (int *)malloc(sizeof(int));
+    *a = 1;
+    int *b = (int *)malloc(sizeof(int));
+    *b = 2;
+    int *c = (int *)malloc(sizeof(int));
+    *c = 3;
+
+    stacks_t *test = stack_init();
+    stack_push(test, (void *)a);
+    assert(test->next != NULL);
+    stack_pop(test);
+    assert(test->next == NULL);
+    stack_push(test, (void *)b);
+    stack_pop(test);
+    stack_push(test, (void *)a);
+    stack_push(test, (void *)b);
+    stack_push(test, (void *)c);
+    int *f = (int *)stack_top(test)->data;
+    assert(*f == 3);
+    stack_clean(test);
+    assert(test->next == NULL);
+    free(test);
+    free(a);
+    free(b);
+    free(c);
+}
+
+stack_avl_t* stacks_avl_init(){
+    stack_avl_t *stacks = (stack_avl_t *)malloc(sizeof(stack_avl_t));
     stacks->node = NULL;
     stacks->next = NULL;
     return stacks;
 }
 
-void stacks_avl_push(stacks_t *stack, avl_node_t *node){
-    stacks_t *new = (stacks_t *)malloc(sizeof(stacks_t));
+void stacks_avl_push(stack_avl_t *stack, avl_node_t *node){
+    stack_avl_t *new = (stack_avl_t *)malloc(sizeof(stack_avl_t));
     if (new != NULL) {
         new->node = node;
         new->next = stack->next;
@@ -20,17 +94,27 @@ void stacks_avl_push(stacks_t *stack, avl_node_t *node){
     }
 }
 
-avl_node_t* stacks_avl_top(stacks_t *stacks){
+avl_node_t* stacks_avl_top(stack_avl_t *stacks){
     if (stacks->next != NULL) {
         return stacks->next->node;
     }
     return NULL;
 }
 
-void stacks_avl_pop(stacks_t *stack){
+void stacks_avl_pop(stack_avl_t *stack){
     if (stack->next != NULL) {
-        stacks_t *first = stack->next;
+        stack_avl_t *first = stack->next;
         stack->next = stack->next->next;
         free(first);
+    }
+}
+
+void stacks_avl_clean(stack_avl_t *stacks){
+    if (stacks != NULL) {
+        while (stacks->next != NULL) {
+            stacks_avl_pop(stacks);
+        }
+    } else {
+        return;
     }
 }
--- stacks.h
+++ stacks.h
@@ -8,16 +8,35 @@
 #include "main.h"
 
 struct stacks_s {
-    avl_node_t       *node;
-    stacks_t         *next;
+    void          *data;
+    stacks_t      *next;
 };
 
-stacks_t* stacks_avl_init();
+struct stack_avl_s {
+    avl_node_t       *node;
+    stack_avl_t         *next;
+};
 
-void stacks_avl_push(stacks_t *stack, avl_node_t *node);
+void stack_test();
 
-avl_node_t* stacks_avl_top(stacks_t *stacks);
+stacks_t* stack_init();
 
-void stacks_avl_pop(stacks_t *stack);
+void stack_push(stacks_t* stack, void *data);
+
+stacks_t* stack_top(stacks_t* stack);
+
+void stack_pop(stacks_t *stack);
+
+void stack_clean(stacks_t *stack);
+
+stack_avl_t* stacks_avl_init();
+
+void stacks_avl_push(stack_avl_t *stack, avl_node_t *node);
+
+avl_node_t* stacks_avl_top(stack_avl_t *stacks);
+
+void stacks_avl_pop(stack_avl_t *stack);
+
+void stacks_avl_clean(stack_avl_t *stacks);
 
 #endif //ALG_STACKS_H
