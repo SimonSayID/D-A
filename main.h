1984d01b7a2c3f0d701c484dba7e0392c8dae24c
simon <SimonSayID@outlook.com>
2017-08-31 11:56:18 +0800

    minor change

--- main.h
+++ main.h
@@ -7,8 +7,12 @@
 
 #include <stdio.h>
 #include <malloc.h>
+#include <sys/param.h>
+#include <assert.h>
 #include "config.h"
+
 #include <stacks.h>
 #include <avl_tree.h>
+#include <rb_tree.h>
 
 #endif //ALG_MAIN_H
