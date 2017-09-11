2c0c80f0a719ae838c4133a9286a660ef4fb50ff
simon <SimonSayID@outlook.com>
2017-09-11 21:37:05 +0800

    add next_prime

--- config.c
+++ config.c
@@ -10,3 +10,30 @@ int max(int a, int b){
     }
     return b;
 }
+
+static int is_prime(size_t num) {
+    for (int i = 3;; i += 2) {
+        size_t q = num / i;
+        if (q < i) {
+            return 1;
+        }
+        if (num == q * i) {
+            return 0;
+        }
+    }
+}
+
+size_t next_prime(size_t num) {
+    if (num < 2) {
+        return 2;
+    }
+    if (!(num & 1)) {
+        num += 1;
+    } else {
+        num += 2;
+    }
+    for (; !is_prime(num); num += 2) {
+
+    }
+    return num;
+}
--- config.h
+++ config.h
@@ -5,12 +5,17 @@
 #ifndef ALG_CONFIG_H
 #define ALG_CONFIG_H
 
-typedef struct queue_n_s queue_n_t;
+#include "os.h"
+
+typedef struct list_s list_t;
 typedef struct queue_s queue_t;
 typedef struct stack_avl_s stack_avl_t;
 typedef struct avl_node_s avl_node_t;
 typedef struct rb_node_s rb_node_t;
+typedef struct hash_table_s hash_table_t;
 
 int max(int a, int b);
+
+size_t next_prime(size_t num);
 
 #endif //ALG_CONFIG_H
